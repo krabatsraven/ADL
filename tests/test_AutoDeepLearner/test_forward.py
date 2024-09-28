bafb6052eb413fd18faeff777c38af47fe93bec6
david <djacoby@uni-mainz.de>
2024-09-28 12:06:42 +0200

    test that output are numbers that are useful: in [0, 1] (because they represent probabilities) and numbers test that new tests should also work after other methods have been called

--- tests/test_AutoDeepLearner/test_forward.py
+++ tests/test_AutoDeepLearner/test_forward.py
@@ -44,10 +44,15 @@ class TestAutoDeepLearnerForward:
             model.forward(x)
 
     def test_forward_form_single_item_batch(self, model: AutoDeepLearner, feature_count: int, class_count: int, msg: str = ''):
-        prediction = model(torch.rand(feature_count))
+        prediction = model(torch.rand(feature_count, requires_grad=True, dtype=torch.float))
+
+        print(prediction)
 
         assert torch.is_tensor(prediction), f'{msg}prediction should be a tensor'
         assert prediction.size() == torch.ones(class_count).size(), f'{msg}prediction should be of the shape (1, nr of classes)'
+        assert prediction.dtype == torch.float, f'{msg}prediction dtype should be float'
+        assert torch.all(prediction >= 0), f'{msg}prediction should be bigger than zero as they represent class probabilities'
+        assert torch.all(prediction >= 0), f'{msg}prediction should smaller than one as they represent class probabilities'
 
     def test_forward_form_multiple_item_batch(self, model: AutoDeepLearner, feature_count: int, class_count: int, batch_size: int = 10_000, msg: str = ''):
         batch_size = random.randint(0, batch_size)
@@ -55,3 +60,7 @@ class TestAutoDeepLearnerForward:
 
         assert torch.is_tensor(prediction), f'{msg}prediction should be a tensor'
         assert prediction.size() == torch.ones(batch_size, class_count).size(), f'{msg}prediction should be of the shape (nr of batches, nr of classes)'
+        assert prediction.dtype == torch.float, f'{msg}prediction dtype should be float'
+        assert torch.all(prediction >= 0), f'{msg}prediction should be bigger than zero as they represent class probabilities'
+        assert torch.all(prediction >= 0), f'{msg}prediction should smaller than one as they represent class probabilities'
+
