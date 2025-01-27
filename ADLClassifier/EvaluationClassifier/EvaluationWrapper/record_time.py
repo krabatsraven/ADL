87067e55bb0f762ad72b4874eb4cc11e964c8247
djacoby <djacoby@students-uni-mainz.de>
2025-01-27 17:30:27 +0100

    wip: add property nr_of_active_layers to ADL

---
+++ ADLClassifier/EvaluationClassifier/EvaluationWrapper/record_time.py
@@ -0,0 +1,29 @@
+import time
+
+from ADLClassifier import ADLClassifier
+
+
+def record_time(adl_classifier: type(ADLClassifier)) -> type(ADLClassifier):
+    class RecordTimeWrapper(adl_classifier):
+        def __str__(self):
+            return f"{super().__str__()}WithTimeRecords"
+
+        def __init__(self):
+            super().__init__()
+
+            self.timings = {
+                "covariance loop": 0,
+            }
+
+        def record_time(self, method, key: str, *args, **kwargs) -> None:
+            time_start = time.time()
+            result = method(*args, **kwargs)
+            time_end = time.time()
+            self.timings[key] += (time_end - time_start)
+            return result
+
+        @property
+        def total_time_in_loop(self):
+            return self.timings["covariance loop"]
+
+    return RecordTimeWrapper
