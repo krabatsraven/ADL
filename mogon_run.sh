1fbdcfb8f775dc81adca7cd3d1c60a5c82399c63
djacoby <djacoby@students-uni-mainz.de>
2025-03-22 13:15:43 +0100

    wip mogon run script

--- mogon_run.sh
+++ mogon_run.sh
@@ -10,13 +10,13 @@
 #------------------------------- Parallelize -------------------------------
 #SBATCH --array=0-152:1
 #------------------------------- Modules -------------------------------
-module load lang/Java
+#module load lang/Java
 #------------------------------- Virtual Environment -------------------------------
 # todo: add python env here
 
 # for CapyMOA
-export MALLOC_ARENA_MAX=4
-export _JAVA_OPTIONS="-Djava.io.tmpdir/localscratch/${SLURM_JOB_ID}/ -Xms50m -Xmx16g -Xss1g"
+#export MALLOC_ARENA_MAX=4
+#export _JAVA_OPTIONS="-Djava.io.tmpdir/localscratch/${SLURM_JOB_ID}/ -Xms50m -Xmx16g -Xss1g"
 
 #------------------------------- Run -------------------------------
 # access job array with environmental variable $SLURM_ARRAY_TASK_ID
