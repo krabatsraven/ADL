6005a3c502f573b9841711bba06520fd0133a3fa
djacoby <djacoby@students-uni-mainz.de>
2025-03-24 15:25:49 +0100

    wip mogon run script

---
+++ mogon_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#========[ + + + + Requirements + + + + ]========#
+#SBATCH --partition=m2_datamining
+#SBATCH --account=ki-topml
+
+#========[ + + + + Job Steps + + + + ]========#
+srun echo "Hello, world!"
+
