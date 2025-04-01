15ba54ba7f9a72d2b9081121b29a389249f1a52c
david <djacoby@uni-mainz.de>
2025-04-01 13:36:57 +0200

    unecessary hello world

--- mogon_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#========[ + + + + Requirements + + + + ]========#
-#SBATCH --partition=m2_datamining
-#SBATCH --account=ki-topml
-
-#========[ + + + + Job Steps + + + + ]========#
-srun echo "Hello, world!"
-
